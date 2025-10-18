71a8309ff63057a34d935a2358af06a600c7ae30
Mohammed <mohammed.ghazy1235@gmail.com>
2025-10-18 15:31:49 +0300

    add new headers file

---
+++ i.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <list>
+#include <cassert>
+#define DEBUG false
+#define ll long long
+#define OJ_DEBUG
+using namespace std;
+
+int main(){
+  
+}
--- p.py
+++
@@ -1,5 +0,0 @@
-
-test = "mohammed";
-
-Array = ['m','o','h']
-
